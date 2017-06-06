0cafda5004e66fd23e2b2a7e95dd3daaa4ee66a4
Aaron Chiu <aaron-chiu22@hotmail.com>
2017-06-06 09:29:14 -0400

    Downloaded placeholder assets and created a test scene.

---
+++ UnityCoverShooter/Assets/Scripts/playaHayta.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playaHayta : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Quaternion theAngle = transform.localRotation;
+            theAngle.Set(1,1,0,1);
+            transform.localRotation = theAngle;
+        }
+    }
+}
